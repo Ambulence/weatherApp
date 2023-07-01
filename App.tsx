a45f74f07c8cc2bc340c127b81834ddd8a094e23
Sasha Kononenko <nurchiksasha@gmail.com>
2023-07-01 18:32:27 +0300

    added lint-staged

--- App.tsx
+++ App.tsx
@@ -6,14 +6,17 @@ export default function App() {
   return (
     <View
 
-        style={styles.container}>
+
+    style={styles.container}>
       <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles
+
+  =     StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
